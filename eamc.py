5542cf51954f7e2df504a5a4369fa746a5fdacc8
Potter <Robin.Potter@experian.com>
2023-03-02 11:18:03 +0000

    transparent single image

--- eamc.py
+++ eamc.py
@@ -1,7 +1,5 @@
 import tkinter
-from PIL import Image, ImageTk
-import io
-from PIL import ImageDraw
+from PIL import Image, ImageTk, ImageDraw
 
 root = tkinter.Tk()
 root.geometry("600x300")
@@ -10,7 +8,7 @@ root.geometry("600x300")
 myCanvas = tkinter.Canvas(root, bg="blue", height=600, width=300) #, tki=None, img=None, draw=None, imc=None)
 
 # new pillow img and attach to canvas
-myCanvas.img = Image.new(size=[600,300],mode="RGB")
+myCanvas.img = Image.new(size=[600,300],mode="RGBA")
 myCanvas.draw = ImageDraw.Draw(myCanvas.img)
 #myCanvas.draw.line((0, 0) + myCanvas.img.size, fill=128)
 #myCanvas.draw.line((0, myCanvas.img.size[1], myCanvas.img.size[0], 0), fill=128)
@@ -44,7 +42,8 @@ def motion(event):
         else:
             #tkimg = ImageTk.PhotoImage(img, height=img.height, width=img.width)
             # update pillow image rather than the image in the canvas
-            myCanvas.draw.line((last[0], last[1], event.x,event.y), fill=255)
+            myCanvas.draw.line((last[0], last[1], event.x,event.y), fill=(255,255,0,255))
+            #i1.paste(i2, mask=i2) 
             # redo the tkimage
             myCanvas.tki = ImageTk.PhotoImage(myCanvas.img, height=myCanvas.img.height, width=myCanvas.img.width)
             myCanvas.create_image((0,0),image=myCanvas.tki, anchor="nw")
